75679d91d1bf96b5a8cd9befd6d01a0413c0c78a
tejas222 <40661110+tejas222@users.noreply.github.com>
2021-08-25 19:40:42 +0530

    Update RedBusBooking.java

--- Multithreading/src/com/RedBusBooking.java
+++ Multithreading/src/com/RedBusBooking.java
@@ -56,7 +56,7 @@ public class RedBusBooking implements Runnable {
 		try {
 			Thread.sleep(5000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 		
